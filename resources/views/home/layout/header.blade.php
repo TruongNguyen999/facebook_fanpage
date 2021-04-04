c6d5834fd1fc4e97559525883c50902a565bcdf6
TruongNguyen999 <nguyentruong061999@gmail.com>
2021-04-04 11:02:23 +0700

    make it better

--- resources/views/home/layout/header.blade.php
+++ resources/views/home/layout/header.blade.php
@@ -2,7 +2,7 @@
 $nodePage = Session::get('nodePage');
 @endphp
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.html">OPTIMUS</a>
+    <a class="navbar-brand" href="{{url('/success')}}">OPTIMUS</a>
     <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
     <!-- Navbar-->
     <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
